8f827bcb40c3e5fc63d813fc2cfba3cef66b631f
BenGuillot <ben.guyot.guillot@gmail.com>
2019-04-09 11:22:15 +0200

    creation du serveur nodejs

--- script.js
+++ script.js
@@ -1,59 +1,14 @@
-import { randomBytes } from "crypto";
+var express = require('express');
+var app = express();
 
-/*var express = require('express');
-var app = express();*/
-
-var btn;
+app.use('/s', express.static('truc'));
 
 
-//initialise une table de la taille de la fenêtre 
-function initWindow(){
-  var section = document.querySelector("#scriptab");
-  section.innerHTML='';
-  var window = document.createElement("table");
-  window.innerHTML='';
-  window.className="window";
-  section.appendChild(window);
-  btn = document.querySelector('.window');
-}
-initWindow();
-
-//ajoute un post it aux coordonnées du double clic 
-function addPostIt(event){
-  console.log("x : "+ event.clientX+" y : "+ event.clientY);
-  var element = document.querySelector(".window");
-  //a enlever si on veut affihcer plusieurs post-it
-  //element.innerHTML = ''; 
-  var table = document.createElement("table");
-  table.innerHTML='';
-  table.style.top = event.clientY + 'px';
-  table.style.left = event.clientX + 'px';
-  table.className="postIt";
-  for(i = 0; i < 2; i++){
-    var tr = document.createElement("tr");
-    tr.innerHTML = '';
-    table.appendChild(tr);
-    var td = document.createElement("td");
-    var textarea = document.createElement("textarea")
-    table.appendChild(textarea);
-    table.appendChild(td);
-  }
-  element.appendChild(table);        
-}
-
-function action(event){
-  alert("creation de post-it!:");
-  addPostIt(event);
-}
-
-btn.addEventListener('dblclick',action);
-
-
-/*app.listen(8080,() =>{
+app.listen(8080,() =>{
   console.log('server started!');
-});*/
+});
 
-//*************************************************** */
+//******************************************************************************************************************************** */
 /*
 app.get('/',function(req,res){
   res.send("Post It!");
---
+++ truc/postit.js
@@ -0,0 +1,43 @@
+var btn;
+
+//initialise une table de la taille de la fenêtre 
+function initWindow(){
+  var section = document.querySelector("#scriptab");
+  section.innerHTML='';
+  var window = document.createElement("table");
+  window.innerHTML='';
+  window.className="window";
+  section.appendChild(window);
+  btn = document.querySelector('.window');
+}
+initWindow();
+
+//ajoute un post it aux coordonnées du double clic 
+function addPostIt(event){
+  console.log("x : "+ event.clientX+" y : "+ event.clientY);
+  var element = document.querySelector(".window");
+  //a enlever si on veut affihcer plusieurs post-it
+  //element.innerHTML = ''; 
+  var table = document.createElement("table");
+  table.innerHTML='';
+  table.style.top = event.clientY + 'px';
+  table.style.left = event.clientX + 'px';
+  table.className="postIt";
+  for(i = 0; i < 2; i++){
+    var tr = document.createElement("tr");
+    tr.innerHTML = '';
+    table.appendChild(tr);
+    var td = document.createElement("td");
+    var textarea = document.createElement("textarea")
+    table.appendChild(textarea);
+    table.appendChild(td);
+  }
+  element.appendChild(table);        
+}
+
+function action(event){
+  alert("creation de post-it!:");
+  addPostIt(event);
+}
+
+btn.addEventListener('dblclick',action);
