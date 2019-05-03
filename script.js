92ccbf8f31d7c7892833a8e7462ea8b2b1289497
BenGuillot <ben.guyot.guillot@gmail.com>
2019-05-03 16:12:55 +0200

    gestion de l'ajout d'un utilisateur

--- script.js
+++ script.js
@@ -3,13 +3,30 @@
 var express = require('express');
 var app = express();
 
+const knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+      filename: "mydb.sqlite3"
+  },
+  useNullAsDefault:true,
+  debug: true,
+});
+const bodyP = require('body-parser');
+const cookieP = require('cookie-parser');
+const consolidate = require('consolidate');
+const expressAsyncErrors = require('express-async-errors');
+
 
 /*
 import { randomBytes } from "crypto";
 */
 
 
-app.use('/s', express.static('app'));
+app.use('/s', express.static('views'))
+  .use(bodyP.urlencoded({ extended: false }))
+  .use(cookieP())
+  .engine('html', consolidate.nunjucks)
+  .set('view engine', 'nunjucks');
 
 
 app.listen(8080,() =>{
@@ -26,8 +43,22 @@ app.get('/signup',function(req,res){
   res.redirect('/s/signup.html');
 });
 
-app.post('/signup',function(req,res){
-  res.send("Sign Up!");
+app.post('/signup',async function(req,res){
+    try{
+      await knex.raw('INSERT INTO users VALUES (?,?)',
+                     [req.body.id, req.body.pwd]);
+    }catch(error){
+      console.error(error);
+      res.redirect('/s/signup.html');
+    }    
+    res.redirect('/userlist');
+  }
+);
+
+/**AFFICHAGE DE LA BD USER POUR LES TESTS */
+app.all('/userlist', async function(req, response) {
+  let str2 = await knex.select('*').from('users');
+  response.render('userlist.html', {'userlist':str2}); 
 });
 
 app.all('/login',function(req,res){
