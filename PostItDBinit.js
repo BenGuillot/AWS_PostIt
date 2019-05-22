ac337d4894b9477af1ca7cf94c501982a63263a9
Glitch (benguillot-aws-postit) <none>
2019-05-22 11:20:11 +0000

    🚓👓 Checkpoint ./script.js:3567769/10 ./PostItDBinit.js:3567769/425

--- PostItDBinit.js
+++ PostItDBinit.js
@@ -23,8 +23,12 @@ async function createPostIt() {
     await knex('postit').columnInfo();
     await knex.raw(`INSERT INTO postit (data,author, x, y, type, protect)
                           VALUES
-                          ('Double click to post, post enough and you may find a shiny post it !', 'FAHEI','150','150','ShinyPostIt','public')`);
+                          ('Double click to post, post enough and you may find a shiny post it !', 'FAHEI','150','150','ShinyPostIt','prive')`);
   
+        await knex.raw(`INSERT INTO postit (data,author, x, y, type, protect)
+                          VALUES
+                          ('Postit privé', 'LAU','10', '100', 'ShinyPostIt', 'public')`);
+    
     console.log(await knex.select('*').from('postit').where('protect',"public"));
   
     await knex.destroy();
--- script.js
+++ script.js
@@ -146,7 +146,7 @@ app.post('/ajouter',async function(req,res){
   //ajout du post it
   try{
     await knex.raw('INSERT INTO postit VALUES (?,?,?,?,?,?,?,?)',
-                    [id,req.body.data, req.body.date, req.body.px, req.body.py, req.session.login, type, "prive");
+                    [id,req.body.data, req.body.date, req.body.px, req.body.py, req.session.login, type, 'prive']);;
   }catch(error){
     console.error(error);
     res.redirect('/');
