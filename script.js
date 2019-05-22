b1f676e9ea2cd4bddfbf6fe03f9c8dcc12575435
Glitch (benguillot-aws-postit) <none>
2019-05-22 18:07:16 +0000

    🏹☺️ Checkpoint ./script.js:3567769/191

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function getRandomInt(max) {
 //HANDLERS
 
 app.all('/',async function(req,res){
-  let postit = await knex.select('*').from('postit');
+  let postit = await knex.select('*').from('postit').where('protect', 'prive');
   res.render(__dirname+'/views/postit.html', {"uid" : uid,
                                               "name" : req.session.login,
                                               "postit" : postit
@@ -80,7 +80,7 @@ app.post('/signup',async function(req,res){
       console.error(error);
       res.redirect('/s/signup.html');
     }    
-    let postit = await knex.select('*').from('postit');
+    let postit = await knex.select('*').from('postit').where('author', req.body.id);
     res.render(__dirname+'/views/postit.html', { "uid" : 1,
                                                  "name" : req.body.id,
                                                  "postit" : postit});
